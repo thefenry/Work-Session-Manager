e351c88453f2b76206140fe77c952b8864470b71
Ivan Sued <thefenry@hotmail.com>
2019-02-13 13:44:44 -0800

    Initial Setup

---
+++ Work Session Manager/Work Session Manager Tool/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work_Session_Manager_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
