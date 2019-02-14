280aa7d3e8a3cba6f18ccb900e12067b139154ad
Ivan Sued <thefenry@gmail.com>
2019-02-13 20:45:02 -0800

    Adding a directory check or creation

--- Work Session Manager/Work Session Manager Tool/MainPage.Designer.cs
+++ Work Session Manager/Work Session Manager Tool/MainPage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Work_Session_Manager_Tool
 {
-    partial class Form1
+    partial class MainPage
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "MainPage";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
---
+++ Work Session Manager/Work Session Manager Tool/MainPage.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace Work_Session_Manager_Tool
+{
+    public partial class MainPage : Form
+    {
+        private const string directoryPath = @"C:\WorkSessionLogs";
+
+        public MainPage()
+        {
+            CheckDirectory();
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Create a directory if it does not already exist
+        /// </summary>
+        private void CheckDirectory()
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+
+    }
+}
