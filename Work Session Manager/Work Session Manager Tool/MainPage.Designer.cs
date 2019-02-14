652b13d04b69c07f1ffe25de1014cef6a8f09f91
Ivan Sued <thefenry@gmail.com>
2019-02-13 20:53:50 -0800

    Adding loging functionality

--- Work Session Manager/Work Session Manager Tool/MainPage.Designer.cs
+++ Work Session Manager/Work Session Manager Tool/MainPage.Designer.cs
@@ -30,39 +30,46 @@
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(328, 93);
+            this.label1.Location = new System.Drawing.Point(323, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(337, 128);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "I just started at this time";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // notifyIcon1
             // 
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 580);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainPage";
-            this.Text = "Form1";
+            this.Text = "Productivity Tracker";
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            this.SizeChanged += new System.EventHandler(this.MainPage_Resize);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
--- Work Session Manager/Work Session Manager Tool/MainPage.cs
+++ Work Session Manager/Work Session Manager Tool/MainPage.cs
@@ -1,4 +1,13 @@
-﻿using System.IO;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Work_Session_Manager_Tool
@@ -12,6 +21,8 @@ namespace Work_Session_Manager_Tool
             CheckDirectory();
 
             InitializeComponent();
+            
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
         }
 
         /// <summary>
@@ -22,6 +33,81 @@ namespace Work_Session_Manager_Tool
             Directory.CreateDirectory(directoryPath);
         }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            string text = $"I just started at: {DateTime.Now.ToLocalTime()} \r\n";
+            WriteToLog(text);
 
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            string text = $"I am closing at: {DateTime.Now.ToLocalTime()} \r\n";
+            WriteToLog(text);
+        }
+
+        void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                //I left my desk;
+                string text = $"I am locking at: {DateTime.Now.ToLocalTime()} \r\n";
+                WriteToLog(text);
+
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                string text = $"I am unlocking at: {DateTime.Now.ToLocalTime()} \r\n";
+                WriteToLog(text);
+            }
+        }
+
+        private void WriteToLog(string text)
+        {
+            label1.Text = text;
+            string path = $@"{directoryPath}\DailyLog-{DateTime.Now.Month}-{DateTime.Now.Day}-{DateTime.Now.Year}.txt";
+            File.AppendAllText(path, text);
+        }
+
+        protected void Displaynotify()
+        {
+            try
+            {
+                notifyIcon1.Icon = new Icon(Path.GetFullPath(@"Images\log.ico"));
+                notifyIcon1.Text = "System Activity Logger Utlity";
+                notifyIcon1.Visible = true;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+            Displaynotify();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void MainPage_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                Displaynotify();
+                this.ShowInTaskbar = false;
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
     }
 }
